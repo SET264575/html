86cbf88281d647a74eba2818e8c524a65ce51c9f
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-02-20 07:27:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
+  this.ctx.fillStyle = '#FFF';
+this.ctx.fillStyle = '#000';
   this.ctx.fillRect(100,100,30,30);
