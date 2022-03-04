00d78da33599faa19aa008f71123085bd52f2bde
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-03-03 19:02:13 -0600

    Add files via upload

---
+++ Work/Radiology/index2.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('#form');
+const input = document.querySelector('#text');
+
+//const re = /ORDER..............(.*?).$IMPRESSION:$(.*?)/mgs;
+const re = /ORDER.{14}(.*?)$.*?IMPRESSION.*?^(.*?)The above report was dictated/mgs;
+
+function convertText(inputText) {
+
+	let array = [...inputText.value.matchAll(re)];
+	let output = 'I have reviewed these radiology images personally.\n';
+	console.log(array);
+	for (let i = 0; i < array.length; i++){
+		let secarray = array[i][1].replace('XR/XR ','X-ray of the ');
+
+		output = output + secarray +' shows: \n'+array[i][2]+'\n';
+	}
+	document.getElementById('out').value = output;	
+
+}
+
+form.addEventListener('submit',(event) => {event.preventDefault(); convertText(input);});
+
