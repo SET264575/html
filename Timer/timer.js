82766f6b2802356fb55d2c9763899c014d6b95b9
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-06-06 00:28:34 -0500

    Create timer.js

---
+++ Timer/timer.js
@@ -0,0 +1,26 @@
+(() => {
+    	const main = document.querySelector('#main');
+    	if (!main.getContext) {
+        	return;
+    	}
+
+    // get the context
+    	ctx = main.getContext('2d');
+//    	main.addEventListener('click',(event) =>{click(event,ctx);});
+
+
+	let id = setInterval(updateClock,10);
+	
+
+	
+
+})();
+
+function updateClock(){
+  t = t + 1;
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0,0,1000,1000);
+  ctx.font = "30px Comic Sans MS";
+	ctx.fillStyle = 'black';
+	ctx.fillText(t,100,400);
+}
