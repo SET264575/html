6ca4d883b32478c7ee365a5868a631ab1b291152
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-09-07 04:18:23 -0500

    Add files via upload

---
+++ Work/chestpain.js
@@ -0,0 +1,64 @@
+      var pts = [];
+      var total = 0;
+
+      function getText2(){
+	pts = [];
+	total = 0;
+	var text = '';
+	createText(getRadioValue('history'),'history');
+	createText(getRadioValue('age'),'age');
+	createText(getRadioValue('EKG'),'EKG');
+	createText(getRadioValue('risk factors'),'risk factors');
+   	createText(getRadioValue('troponin'),'troponin');
+	text = combine(pts);
+	text = "HEART score was " + total + " with " + text + ". ";
+	if (total < 4){
+		text = text + "With a HEART score in this range, the risk of major adverse cardiac event within the next 6 weeks is less than 2%.";
+	}
+	if (total > 3) {
+		text = text + "Given the risk of a major adverse cardiac event, the patient was admitted.";
+	}
+	console.log(text);
+      }
+
+      function getRadioValue(id) {
+            var ele = document.getElementsByName(id);
+            var value = "";
+            for(i = 0; i < ele.length; i++) {
+                if(ele[i].checked){
+                	value = ele[i].value;
+			console.log(value);
+		}
+            }
+	return(value);
+        }
+
+      function createText(value,id){
+	var t = '';
+	switch(value) {
+		case '0':
+			t = '';
+			break;
+		case '1':
+			t = '1 point for '+ id;
+			pts.push(t);
+			total++;
+			break;
+		case '2':
+			t = '2 points for ' + id;
+			pts.push(t);
+			total = total + 2;
+			break;
+	  }
+	}
+
+	function combine(x){
+		var t = x[0];
+		for (let i =1; i < x.length-1;i++){
+			t = t + ", "+x[i];
+		}
+		if (x.length > 1) {
+			t = t + " and "+x[x.length-1];
+		}
+		return(t);
+	}
---
+++ Work/sepsis.js
@@ -0,0 +1,51 @@
+function copy2(){
+	console.log("in copy");
+	var text = "";
+	const SIRS = [];
+	var lactate = "";
+	var cause = "";
+	var treatment = [];
+
+	if (getValue('leukocytosis')) {SIRS.push("leukocytosis");}
+	if (getValue('fever')) {SIRS.push("fever");}
+	if (getValue('tachycardia')) {SIRS.push("tachycardia");}
+	if (getValue('tachypnea')) {SIRS.push("tachypnea");}
+
+	text = "The patient meets SIRS criteria with "+ combine(SIRS) + ".";
+
+	if (getValue('highlactate')) {lactate = "elevated";}
+	if (getValue("lowlactate")) {lactate = "notelevated";}
+
+	text = text + " Lactate was " + lactate + ".";
+
+	text = text + " The patient appears to be septic due to " + document.getElementById('cause').value+'. ';
+
+	if (getValue('IVF')) {treatment.push("IV normal saline");}
+	if (getValue('IVF30')) {treatment.push("IV normal saline at 30 cc/kg");}
+	if (getValue('Rocephin')) {treatment.push("Rocephin");}
+	if (getValue('Vanc')) {treatment.push("Vancomycin");}
+	if (getValue('Zosyn')) {treatment.push("Zosyn");}
+	if (getValue('Zithromax')) {treatment.push("Zithromax");}
+	if (getValue('Cipro')) {treatment.push("Cipro");}
+	if (getValue('Flagyl')) {treatment.push("Flagyl");}
+
+	text = text + "Treatment initiated in the ER included " + combine(treatment) +'.';
+
+	document.getElementById('textarea').value = text;
+	navigator.clipboard.writeText(text);
+}
+
+function getValue(id){
+	return(document.getElementById(id).checked);
+}
+
+function combine(x){
+	var t = x[0];
+	for (let i =1; i < x.length-1;i++){
+		t = t + ", "+x[i];
+	}
+	if (x.length > 1) {
+		t = t + " and "+x[x.length-1];
+	}
+	return(t);
+}
