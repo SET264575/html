d3ba27f5774c0892ecc144db2ef89cb8189fa2ce
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-09-15 12:50:41 -0500

    Update Electrolyte.js

--- Work/References/Electrolyte.js
+++ Work/References/Electrolyte.js
@@ -23,6 +23,9 @@ syndromes.push( new Syndrome("facticious hyponatremia",[1,0,1]));
 syndromes.push(new Syndrome("hyponatremia",[-1,0,0]));
 syndromes.push(new Syndrome("Addisonian crisis",[-1,1,0]));
 
+function changeResult(name,value){
+}
+	
 var testResults = [-1,1,0];
 for (let i = 0; i<syndromes.length;i++){
 	console.log(syndromes[i].name,syndromes[i].score(testResults));
