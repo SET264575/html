a9508076b9689ae91f4ca94a875158355de45f08
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-11-06 22:13:03 -0600

    Update tree.js

--- Work/DDX/tree.js
+++ Work/DDX/tree.js
@@ -3,6 +3,7 @@ var i;
 
 for (i = 0; i < toggler.length; i++) {
   toggle[i].parentElement.querySelector(".nested").classList.toggle("active");
+  toggle[i].classList.toggle("caret-down");
   toggler[i].addEventListener("click", function() {
     this.parentElement.querySelector(".nested").classList.toggle("active");
     this.classList.toggle("caret-down");
