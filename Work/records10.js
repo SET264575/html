c035be895421d9a32a71e57f26bf43359729269e
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-11-13 17:59:27 -0600

    Update records10.js

--- Work/records10.js
+++ Work/records10.js
@@ -634,14 +634,13 @@ function pickRoom(i){
 }
 
 function deleteRoom(i){
-  room[i].mine = false;
-  toDoList[i] = [];
   all_dx = all_dx + "\n"+document.getElementById("rm"+String(i)+"_dx").value;
   all_plans = all_plans + "\n" + document.getElementById("rm"+String(i)+"_plan").value;
+  room[i].mine = false;
+  toDoList[i] = [];
   document.getElementById('dx').value = all_dx;
-  document.getElementById('plans').value = all_plans;
+  document.getElementById('plans').value = "test"; //all_plans;
 	
- 
   refreshTable();
   displayRecords();
 }
