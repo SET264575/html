872c5e28ca117cc168b547bbbf85d41c30940174
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-11-17 22:08:19 -0600

    Update labanalyzer2.js

--- Work/DDX/labanalyzer2.js
+++ Work/DDX/labanalyzer2.js
@@ -733,7 +733,7 @@ printAbnormalities();
 		//console.log(abnormalities);
 		createDiseaseList(abnormalities);
 		developDifferential(abnormalities);		
-		setTimeout(tree,15000);
+		setTimeout(tree,8000);
 	}	
 
 	function createDiseaseList(abnormalities){
