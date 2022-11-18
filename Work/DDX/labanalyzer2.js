693fc7d94106733d2d09c748ffe475ad3dda1644
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-11-17 21:52:53 -0600

    Update labanalyzer2.js

--- Work/DDX/labanalyzer2.js
+++ Work/DDX/labanalyzer2.js
@@ -891,7 +891,9 @@ printAbnormalities();
 		var acid_basestatus = "normal";
 		var process = "normal acid/base status";
 		var compensation = "none";
-		
+		if (pH == NaN || pO2 == NaN || pCO2 == NaN || HCO3 == NaN || BE == NaN) {
+			return("incomplete data");
+		}
 		//oxygenation
 		if (pO2 < 75) {
 			oxygenation = "hypoxemic";
