8504a527342527ceb17349d52411c8c15155cbfa
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-11-23 20:18:27 -0600

    Add files via upload

--- Work/DDX/tree.js
+++ Work/DDX/tree.js
@@ -2,11 +2,16 @@ var toggler = document.getElementsByClassName("caret");
 var i;
 
 for (i = 0; i < toggler.length; i++) {
-//  toggler[i].parentElement.querySelector(".nested").classList.toggle("active");//
-//  toggler[i].classList.toggle("caret-down");
   toggler[i].addEventListener("click", function() {
-    console.log(this);
+	console.log(this);
     this.parentElement.querySelector(".nested").classList.toggle("active");
     this.classList.toggle("caret-down");
   });
+}
+
+function showAll(){
+	for (let j = 0; j < toggler.length; j++) {
+		toggler[j].parentElement.querySelector(".nested").classList.add("active");
+		toggler[j].classList.add("caret-down");
+	}
 }
