66e864be6002036b715210db4e4ddf8ee2ae1183
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-11-28 05:49:25 -0600

    Update labanalyzer2.js

--- Work/DDX/labanalyzer2.js
+++ Work/DDX/labanalyzer2.js
@@ -1212,7 +1212,7 @@ Ur Bact trace
 			{
 				console.log(b);
 				for (let i = 0; i < labs.length; i++){
-					//console.log(b[1].trim(),labs[i].symbol);
+					console.log(b[1].trim(),labs[i].symbol);
 					if (b[1].trim() == labs[i].symbol) {document.getElementById(labs[i].name).value = b[2];}
 				}
 			}
