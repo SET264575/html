355d2820d0d2784edd5c67f193fd8909a1759dc8
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-12-22 21:34:55 -0600

    Update physicians.js

--- Work/MDM/physicians.js
+++ Work/MDM/physicians.js
@@ -10,17 +10,17 @@ function getObject(website){
 function displayAdmitting() {
 	var t = "";
 	for (let i =0; i<physObj.admitting.length; i++) {
-    t = t + '<button class="collapsible subsubheadingbutton">'+physObj.admitting[i].specialty+'</button>';
-    t = t +'      <div class = "content">';
-    t = t + '      <ul class="radiobutton">';
+    t = t + '<button class="collapsible subsubheadingbutton">'+physObj.admitting[i].specialty+'</button>\n';
+    t = t +'      <div class = "content">\n';
+    t = t + '      <ul class="radiobutton">\n';
 
     for (let j = 0; j < physObj.admitting[i].physician.length; j++) {
       let phys = physObj.admitting[i].physician[j];
-      t = t +  ' <li><input type = "checkbox" id="Dr. '+ phys + '" name= "admitting" onclick="check(this.id)" value="Dr. '+phys + '"><label for = "Dr. '+phys+'">Dr. '+phys+'</label></li>'
+      t = t +  ' <li><input type = "checkbox" id="Dr. '+ phys + '" name= "admitting" onclick="check(this.id)" value="Dr. '+phys + '"><label for = "Dr. '+phys+'">Dr. '+phys+'</label></li>\n'
     }
 	  t = t + "</ul></div><br><br>\n";
 	}
-	t = t + "</ul>";
+	//t = t + "</ul>\n";
 	document.getElementById('admittingbuttons').innerHTML =  t;
 	console.log(t);
 }
@@ -28,17 +28,17 @@ function displayAdmitting() {
 function displayConsulting() {
 	var t = "";
 	for (let i =0; i<physObj.consulting.length; i++) {
-    t = t + '<button class="collapsible subsubheadingbutton">'+physObj.consulting[i].specialty+'</button>';
-    t = t +'      <div class = "content">';
-    t = t + '      <ul class="radiobutton">';
+    t = t + '<button class="collapsible subsubheadingbutton">'+physObj.consulting[i].specialty+'</button>\n';
+    t = t +'      <div class = "content">\n';
+    t = t + '      <ul class="radiobutton">\n';
 
     for (let j = 0; j < physObj.consulting[i].physician.length; j++) {
       let phys = physObj.consulting[i].physician[j];
-      t = t +  ' <li><input type = "checkbox" id="consultingDr. '+ phys + '" name= "consulting" onclick="check(this.id)" value="Dr. '+phys + '"><label for = "consultingDr. '+phys+'">Dr. '+phys+'</label></li>'
+      t = t +  ' <li><input type = "checkbox" id="consultingDr. '+ phys + '" name= "consulting" onclick="check(this.id)" value="Dr. '+phys + '"><label for = "consultingDr. '+phys+'">Dr. '+phys+'</label></li>\n'
     }
 	  t = t + "</ul></div><br><br>\n";
 	}
-	t = t + "</ul>";
+	//t = t + "</ul>";
 	document.getElementById('consultingbuttons').innerHTML =  t;
 	console.log(t)
 }
