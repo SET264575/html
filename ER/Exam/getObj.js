29b13edb60f81bd5d403679be1d451b3b768dfc1
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-12-30 04:56:24 -0600

    Update getObj.js

--- ER/Exam/getObj.js
+++ ER/Exam/getObj.js
@@ -1,7 +1,7 @@
 var myObj = {};
 function getObj(website){
       const xmlhttp = new XMLHttpRequest();
-      xmlhttp.onload = function() { myObj = JSON.parse(this.responseText); console.log(myObj)};
+      xmlhttp.onload = function() { myObj = JSON.parse(this.responseText); console.log(myObj), return(myObj)};
       xmlhttp.open("GET",website);
       xmlhttp.send();
     }
