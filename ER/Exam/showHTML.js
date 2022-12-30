f082ef38a25032b684ae8a07d3cadddbe0718837
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-12-30 04:56:46 -0600

    Update showHTML.js

--- ER/Exam/showHTML.js
+++ ER/Exam/showHTML.js
@@ -1,4 +1,4 @@
-function showHTML() {
+function showHTML(myObj) {
   var t = "<ul class = 'radiobutton'>";
   for (let i = 0; i<myObj.button.length; i++) {
     t = t + "<li><input type = 'checkbox' value = '"+myObj.button[i] + "' onclick= 'check(this.id)'><label for = '"+myObj.button[i]+"'>"+myObj.button[i]+"</label>";
