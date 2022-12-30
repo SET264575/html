5261ecd2b11e84d94353368252f63473afc2d5b8
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-12-30 03:43:59 -0600

    Update getObj.js

--- ER/Exam/getObj.js
+++ ER/Exam/getObj.js
@@ -1,9 +1,8 @@
 var myObj = {};
 getObject('neuro.json');
-console.log('test");
 function getObject(website){
       const xmlhttp = new XMLHttpRequest();
-      xmlhttp.onload = function() { myObj = JSON.parse(this.responseText); console.log("test");console.log(myObj)};
+      xmlhttp.onload = function() { myObj = JSON.parse(this.responseText); console.log(myObj)};
       xmlhttp.open("GET",website);
       xmlhttp.send();
     }
