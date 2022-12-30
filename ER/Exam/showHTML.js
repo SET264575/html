c6ea6b676fe89a323ffb962baafb14a694ec0bd5
SET264575 <100035024+SET264575@users.noreply.github.com>
2022-12-30 04:38:04 -0600

    Update showHTML.js

--- ER/Exam/showHTML.js
+++ ER/Exam/showHTML.js
@@ -1,7 +1,7 @@
 function showHTML() {
   var t = "<ul class = 'radiobutton'>";
   for (let i = 0; i<myObj.group.length; i++) {
-    t = t + "<li><input type = 'checkbox' value = '"+myObj.group[i].label + "'><label for = '"+myObj.group[i].label+"'>"+myObj.group[i].label+"</label>";
+    t = t + "<li><input type = 'checkbox' value = '"+myObj.button[i] + "'><label for = '"+myObj.button[i]+"'>"+myObj.button[i]+"</label>";
     
     
   }
