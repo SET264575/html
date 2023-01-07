0bb761e421545cf31d92ffcb1387022a6d5d5b8f
SET264575 <100035024+SET264575@users.noreply.github.com>
2023-01-07 10:15:06 -0600

    Update procedure.js

--- Work/Procedures/procedure.js
+++ Work/Procedures/procedure.js
@@ -9,19 +9,26 @@ function getRadioButtonValue(name){
 }
 
 function getValue(id) {
-  var type = document.getElementById(id).type;
+  var x = document.getElementById(id);
   var result = false;
-  if (type == 'checkbox') {
-    result = document.getElementById(id).checked;
-  }
-  if (type == "radio") {
+  if (x == null) {
     result = getRadioButtonValue(id);
+    console.log('treating ' + id + " as a radio button with result = " + result);
   }
-  if (type = "text") {
-    result = document.getElementById(id).value;
-  }
-  if (type = "select") {
-    result = document.getElementById(id).value;
+  else {
+    var type = x.type;
+    if (type == 'checkbox') {
+      result = document.getElementById(id).checked;
+      console.log('treating ' + id + " as a checkbox with result = " + result);
+    }
+    if (type == "text") {
+      result = document.getElementById(id).value;
+      console.log('treating ' + id + " as a textbox with result = " + result);
+    }
+    if (type ==select-one") {
+      result = document.getElementById(id).value;
+      console.log('treating ' + id + " as a select with result = " + result);
+    }
   }
   return(result);
 }
