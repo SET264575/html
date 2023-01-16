f3416d0a3b56e07881284caf8b5b51afd4ae36b0
SET264575 <100035024+SET264575@users.noreply.github.com>
2023-01-15 21:21:33 -0600

    Update procedure.js

--- Work/Procedures/procedure.js
+++ Work/Procedures/procedure.js
@@ -170,11 +170,12 @@ function woundpreparation() {load('woundpreparation')}
 
 function getParameter(p) {
   console.log(p);
+  console.log(window.location.search);
   const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
   });
   // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-  console.log(params[p]);
+  console.log(params,params[p]);
   return( params[p]);
 }
 
