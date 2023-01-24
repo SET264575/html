6bf98047e4e0565c1d67e04156eba72e7b50b88f
SET264575 <100035024+SET264575@users.noreply.github.com>
2023-01-24 12:06:43 -0600

    Update copyFormatted.js

--- ER/Documenting/copyFormatted.js
+++ ER/Documenting/copyFormatted.js
@@ -1,5 +1,5 @@
 function copyFormatted(formattedText) {
-  const formattedText = 'This is a <b>bold</b> and <u>underlined</u> test.';
+
   const blob = new Blob([formattedText], {type: 'text/html'});
   const clipboardItem = new window.ClipboardItem({ 'text/html': blob });
   navigator.clipboard.write([clipboardItem]);
