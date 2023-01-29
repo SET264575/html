a57fb6791cb079b156fc731160efa0c45dea15bb
SET264575 <100035024+SET264575@users.noreply.github.com>
2023-01-28 20:11:10 -0600

    Create discharge.js

---
+++ Work/Discharge/discharge.js
@@ -0,0 +1,6 @@
+function copyFormatted(formattedText) {
+ // const formattedText = 'This is a <b>bold</b> and <u>underlined</u> test.';
+  const blob = new Blob([formattedText], {type: 'text/html'});
+  const clipboardItem = new window.ClipboardItem({ 'text/html': blob });
+  navigator.clipboard.write([clipboardItem]);
+}
