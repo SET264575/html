01a296c5d4907823c06a2670122d18813ee26aac
SET264575 <100035024+SET264575@users.noreply.github.com>
2023-02-03 12:09:06 -0600

    Create dropdown.js

---
+++ Work/Procedures/dropdown.js
@@ -0,0 +1,209 @@
+
+const selected = [];
+
+synonym = {};
+synonym['abdomen'] = ['abdomen'];
+synonym['ankle'] = ['ankle'];
+synonym['anterior neck'] = ['anterior neck'];
+synonym['buttock'] = ['buttock'];
+synonym['calf'] = ['calf'];
+synonym['cheek'] = ['cheek'];
+synonym['chest'] = ['chest'];
+synonym['dorsum of hand'] = ['dorsum of hand'];
+synonym['elbow'] = ['elbow'];
+synonym['epigastric region'] = ['epigastric region'];
+synonym['epigastrium'] = ['epigastrium'];
+synonym['eyebrow'] = ['eyebrow'];
+synonym['face'] = ['face'];
+synonym['fingers'] = ['fingers'];
+synonym['forearm'] = ['forearm'];
+synonym['forefoot'] = ['forefoot'];
+synonym['forehead'] = ['forehead'];
+synonym['great toe'] = ['great toe'];
+synonym['hand'] = ['hand'];
+synonym['heel'] = ['heel'];
+synonym['hip'] = ['hip'];
+synonym['inner thigh'] = ['inner thigh'];
+synonym['knee'] = ['knee'];
+synonym['lateral malleolus'] = ['lateral malleolus'];
+synonym['lateral thigh'] = ['lateral thigh'];
+synonym['left abdomen'] = ['left abdomen'];
+synonym['left ankle'] = ['left ankle'];
+synonym['left anterior neck'] = ['left anterior neck'];
+synonym['left buttock'] = ['left buttock'];
+synonym['left calf'] = ['left calf'];
+synonym['left cheek'] = ['left cheek'];
+synonym['left chest'] = ['left chest'];
+synonym['left dorsum of hand'] = ['left dorsum of hand'];
+synonym['left elbow'] = ['left elbow'];
+synonym['left eyebrow'] = ['left eyebrow'];
+synonym['left face'] = ['left face'];
+synonym['left fingers'] = ['left fingers'];
+synonym['left forearm'] = ['left forearm'];
+synonym['left forefoot'] = ['left forefoot'];
+synonym['left forehead'] = ['left forehead'];
+synonym['left great toe'] = ['left great toe'];
+synonym['left hand'] = ['left hand'];
+synonym['left heel'] = ['left heel'];
+synonym['left hip'] = ['left hip'];
+synonym['left inner thigh'] = ['left inner thigh'];
+synonym['left knee'] = ['left knee'];
+synonym['left lateral malleolus'] = ['left lateral malleolus'];
+synonym['left lateral thigh'] = ['left lateral thigh'];
+synonym['left left lower quadrant'] = ['left left lower quadrant'];
+synonym['left left upper quadrant'] = ['left left upper quadrant'];
+synonym['left lip'] = ['left lip'];
+synonym['left lower back'] = ['left lower back'];
+synonym['left lower leg'] = ['left lower leg'];
+synonym['left lower lip'] = ['left lower lip'];
+synonym['left lower quadrant'] = ['left lower quadrant'];
+synonym['left medial malleolus'] = ['left medial malleolus'];
+synonym['left midfoot'] = ['left midfoot'];
+synonym['left neck'] = ['left neck'];
+synonym['left nose'] = ['left nose'];
+synonym['left occiput'] = ['left occiput'];
+synonym['left occiputal scalp'] = ['left occiputal scalp'];
+synonym['left palm'] = ['left palm'];
+synonym['left parietal scalp'] = ['left parietal scalp'];
+synonym['left perineum'] = ['left perineum'];
+synonym['left periumbilical region'] = ['left periumbilical region'];
+synonym['left posterior neck'] = ['left posterior neck'];
+synonym['left right lower quadrant'] = ['left right lower quadrant'];
+synonym['left right upper quadrant'] = ['left right upper quadrant'];
+synonym['left sacrum'] = ['left sacrum'];
+synonym['left scalp'] = ['left scalp'];
+synonym['left shoulder'] = ['left shoulder'];
+synonym['left shoulder blade'] = ['left shoulder blade'];
+synonym['left thigh'] = ['left thigh'];
+synonym['left thoracic back'] = ['left thoracic back'];
+synonym['left toes'] = ['left toes'];
+synonym['left tongue'] = ['left tongue'];
+synonym['left upper arm'] = ['left upper arm'];
+synonym['left upper extremity'] = ['left upper extremity'];
+synonym['left upper lip'] = ['left upper lip'];
+synonym['left upper quadrant'] = ['left upper quadrant'];
+synonym['left wrist'] = ['left wrist'];
+synonym['lip'] = ['lip'];
+synonym['lower back'] = ['lower back'];
+synonym['lower leg'] = ['lower leg'];
+synonym['lower lip'] = ['lower lip'];
+synonym['medial malleolus'] = ['medial malleolus'];
+synonym['midfoot'] = ['midfoot'];
+synonym['neck'] = ['neck'];
+synonym['nose'] = ['nose'];
+synonym['occiput'] = ['occiput'];
+synonym['occiputal scalp'] = ['occiputal scalp'];
+synonym['palm'] = ['palm'];
+synonym['parietal scalp'] = ['parietal scalp'];
+synonym['perineum'] = ['perineum'];
+synonym['periumbilical region'] = ['periumbilical region'];
+synonym['posterior neck'] = ['posterior neck'];
+synonym['right abdomen'] = ['right abdomen'];
+synonym['right ankle'] = ['right ankle'];
+synonym['right anterior neck'] = ['right anterior neck'];
+synonym['right buttock'] = ['right buttock'];
+synonym['right calf'] = ['right calf'];
+synonym['right cheek'] = ['right cheek'];
+synonym['right chest'] = ['right chest'];
+synonym['right dorsum of hand'] = ['right dorsum of hand'];
+synonym['right elbow'] = ['right elbow'];
+synonym['right eyebrow'] = ['right eyebrow'];
+synonym['right face'] = ['right face'];
+synonym['right fingers'] = ['right fingers'];
+synonym['right forearm'] = ['right forearm'];
+synonym['right forefoot'] = ['right forefoot'];
+synonym['right forehead'] = ['right forehead'];
+synonym['right great toe'] = ['right great toe'];
+synonym['right hand'] = ['right hand'];
+synonym['right heel'] = ['right heel'];
+synonym['right hip'] = ['right hip'];
+synonym['right inner thigh'] = ['right inner thigh'];
+synonym['right knee'] = ['right knee'];
+synonym['right lateral malleolus'] = ['right lateral malleolus'];
+synonym['right lateral thigh'] = ['right lateral thigh'];
+synonym['right left lower quadrant'] = ['right left lower quadrant'];
+synonym['right left upper quadrant'] = ['right left upper quadrant'];
+synonym['right lip'] = ['right lip'];
+synonym['right lower back'] = ['right lower back'];
+synonym['right lower leg'] = ['right lower leg'];
+synonym['right lower lip'] = ['right lower lip'];
+synonym['right lower quadrant'] = ['right lower quadrant'];
+synonym['right medial malleolus'] = ['right medial malleolus'];
+synonym['right midfoot'] = ['right midfoot'];
+synonym['right neck'] = ['right neck'];
+synonym['right nose'] = ['right nose'];
+synonym['right occiput'] = ['right occiput'];
+synonym['right occiputal scalp'] = ['right occiputal scalp'];
+synonym['right palm'] = ['right palm'];
+synonym['right parietal scalp'] = ['right parietal scalp'];
+synonym['right perineum'] = ['right perineum'];
+synonym['right periumbilical region'] = ['right periumbilical region'];
+synonym['right posterior neck'] = ['right posterior neck'];
+synonym['right right lower quadrant'] = ['right right lower quadrant'];
+synonym['right right upper quadrant'] = ['right right upper quadrant'];
+synonym['right sacrum'] = ['right sacrum'];
+synonym['right scalp'] = ['right scalp'];
+synonym['right shoulder'] = ['right shoulder'];
+synonym['right shoulder blade'] = ['right shoulder blade'];
+synonym['right thigh'] = ['right thigh'];
+synonym['right thoracic back'] = ['right thoracic back'];
+synonym['right toes'] = ['right toes'];
+synonym['right tongue'] = ['right tongue'];
+synonym['right upper arm'] = ['right upper arm'];
+synonym['right upper extremity'] = ['right upper extremity'];
+synonym['right upper lip'] = ['right upper lip'];
+synonym['right upper quadrant'] = ['right upper quadrant'];
+synonym['right wrist'] = ['right wrist'];
+synonym['sacrum'] = ['sacrum'];
+synonym['scalp'] = ['scalp'];
+synonym['shoulder'] = ['shoulder'];
+synonym['shoulder blade'] = ['shoulder blade'];
+synonym['thigh'] = ['thigh'];
+synonym['thoracic back'] = ['thoracic back'];
+synonym['toes'] = ['toes'];
+synonym['tongue'] = ['tongue'];
+synonym['upper arm'] = ['upper arm'];
+synonym['upper extremity'] = ['upper extremity'];
+synonym['upper lip'] = ['upper lip'];
+synonym['vertex'] = ['vertex'];
+synonym['wrist'] = ['wrist'];
+
+
+dict = {};
+for (x of Object.keys(synonym)) {
+	for (y of synonym[x]) {
+		dict[y] = x;
+	}
+}
+
+symptomlist = Object.keys(dict);
+symptomlist.sort();
+
+var t = "";
+for (let i = 0; i<symptomlist.length; i++) {
+  t = t + '<option>'+symptomlist[i]+'</option>';
+}
+document.getElementById('location_list').innerHTML = t;
+
+const input = document.getElementById('location');
+input.addEventListener('keyup',(event) => {
+	if (event.keyCode === 13) {
+		let prefix = document.getElementById('location').value;
+		let elm = document.getElementById('location_list').firstElementChild;
+		let count = 0;
+		let q = "";
+		while (elm) {
+			if (elm.value.startsWith(prefix)) {
+				count++;
+				q = elm.value;
+			}
+		
+			elm = elm.nextElementSibling;
+		}
+		if (count == 1) {
+			document.getElementById('location').value = '';
+			selected.push(dict[q]);
+			//document.getElementById('selected').innerHTML = document.getElementById('selected').innerHTML + '\n'+dict[q];
+		}
+	}
+});
