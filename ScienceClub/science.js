1cfa1260c5599200a0f1bb6843cd58a3bfee5ad2
SET264575 <100035024+SET264575@users.noreply.github.com>
2023-10-18 06:25:57 -0500

    Update science.js

--- ScienceClub/science.js
+++ ScienceClub/science.js
@@ -1,5 +1,9 @@
-document.getElementById('divHeader').innerHTML = "<div width='100%' style='position: fixed; top: 0'>\
+var x = document.getElementById('divHeader')
+consol.log(x)
+if (document.getElementById('divHeader')) {
+  document.getElementById('divHeader').innerHTML = "<div width='100%' style='position: fixed; top: 0'>\
   <a href='home.html'><img src='Granny-Logo.svg' width='30%' >\
   </a></div>"
+}
 document.getElementById('divHeader2').innerHTML = "<div width='100%' style='position: fixed; top: 0'>\
   <a href='../home.html'><img src='Granny-Logo.svg' width='30%'></a>"
