3a6aec8fb2cf102ea541dc2503a5ef49ce6c72b6
SET264575 <100035024+SET264575@users.noreply.github.com>
2023-10-18 06:26:59 -0500

    Update science.js

--- ScienceClub/science.js
+++ ScienceClub/science.js
@@ -1,6 +1,5 @@
-var x = document.getElementById('divHeader')
-consol.log(x)
-if (document.getElementById('divHeader')) {
+
+if (document.getElementById('divHeader') != null) {
   document.getElementById('divHeader').innerHTML = "<div width='100%' style='position: fixed; top: 0'>\
   <a href='home.html'><img src='Granny-Logo.svg' width='30%' >\
   </a></div>"
