4d2711d1a1ae1967e649f2690cab736901a97671
SET264575 <100035024+SET264575@users.noreply.github.com>
2023-10-18 05:16:59 -0500

    Update science.js

--- ScienceClub/science.js
+++ ScienceClub/science.js
@@ -1,2 +1,2 @@
-document.getElementById('divHeader').innerHTML = "<a href='home.html'><img src='Granny-Logo.svg' width='30%'></a>"
+document.getElementById('divHeader').innerHTML = "<a href='home.html'><img src='Granny-Logo.svg' width='30%' style='position: fixed; top: 0'></a>"
 document.getElementById('divHeader2').innerHTML = "<a href='../home.html'><img src='Granny-Logo.svg' width='30%'></a>"
