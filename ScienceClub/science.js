8485f082b2f6fbbc5922927a5f48f22fb7fd67c0
SET264575 <100035024+SET264575@users.noreply.github.com>
2023-10-18 11:30:29 -0500

    Update science.js

--- ScienceClub/science.js
+++ ScienceClub/science.js
@@ -5,7 +5,7 @@ if (document.getElementById('divHeader') != null) {
   <a href='home.html'><img src='Granny-Logo.svg' width='60%' >\
   </a></div>"
 }
-document.getElementById('divHeader2').innerHTML = "<div width="100%"><div width='100%' style='position: fixed; top: 0; padding-bottom: 20px;\
+document.getElementById('divHeader2').innerHTML = "<div style='position: fixed; top: 0; left: 0; right: 0; padding-bottom: 20px;\
   z-index: 100; opacity: 100%; background: white;'>\
   <a href='../home.html'><img src='../Granny-Logo.svg' width='60%'>\
   </a></div></div>"
