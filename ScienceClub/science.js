5c7466e0032ba532e1e278e69d62c1948ac130f5
SET264575 <100035024+SET264575@users.noreply.github.com>
2023-11-02 17:00:59 -0500

    Update science.js

--- ScienceClub/science.js
+++ ScienceClub/science.js
@@ -5,7 +5,15 @@ if (document.getElementById('divHeader') != null) {
   <a href='home.html'><img src='Granny-Logo.svg' height='60px' >\
   </a></div>"
 }
-document.getElementById('divHeader2').innerHTML = "<div style='position: fixed; top: 0; left: 0; right: 0; padding-bottom: 20px;\
-  z-index: 100; opacity: 100%; background: white;'>\
-  <a href='../home.html'><img src='../Granny-Logo.svg' height='60px'>\
-  </a></div></div>"
+if (document.getelementById('divHeader2') != null) {
+  document.getElementById('divHeader2').innerHTML = "<div style='position: fixed; top: 0; left: 0; right: 0; padding-bottom: 20px;\
+    z-index: 100; opacity: 100%; background: white;'>\
+    <a href='../home.html'><img src='../Granny-Logo.svg' height='60px'>\
+    </a></div></div>"
+}
+if (document.getelementById('divHeader3') != null) {
+  document.getElementById('divHeader3').innerHTML = "<div style='position: fixed; top: 0; left: 0; right: 0; padding-bottom: 20px;\
+    z-index: 100; opacity: 100%; background: white;'>\
+    <a href='../../home.html'><img src='../../Granny-Logo.svg' height='60px'>\
+    </a></div></div>"
+}
