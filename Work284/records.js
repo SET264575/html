aedc2ff407247ceb6cde390fdadf074331d34483
SET264575 <100035024+SET264575@users.noreply.github.com>
2024-03-25 09:21:01 -0500

    changed

---
+++ Work284/records.js
@@ -0,0 +1,249 @@
+//Improvements:
+
+// * Put work up and note in 2 columns
+// done - Add textbox for dx, problems
+//  Pending labs:  UA, repeat troponin
+//  *Redraw when room selected or box checked - don't reload, will erase data.  
+//  Indent labs, etc
+//  timing of events
+//  save to file
+//  release room with button
+
+function Room() {
+  this.mine = false;
+  this.wu_orders =  true;
+  this.wu_workuppending =  true;
+  this.wu_labspending =  true;
+  this.wu_imagingpending =  true;
+  this.wu_procedurepending = true;
+  this.wu_consult = true;
+  this.wu_disposition = false;
+  this.admit_bedrequest = true;
+  this.admit_discharge = true;
+  this.admit_order = true;
+  this.discharge_dx = true;
+  this.discharge_instructions = true;
+  this.discharge_prescriptions = true;
+  this.discharge_referral = true;
+  this.discharge_outptorders = true;
+  this.discharge_worknote = true;
+  this.wu_problems = "";
+  this.wu_dx = "";
+  this.note_hpi = true;
+  this.note_ros = true;
+  this.note_pe = true;
+  this.note_course = true;
+  this.note_consult = true;
+  this.note_mdm = true;
+  this.note_ekg = true;
+  this.note_disposition = true;
+  this.note_sign = true;
+}
+
+const room = [];
+
+for (let i = 1; i < 17; i++){
+  room[i] = new Room;
+}
+
+room[3].mine = true;
+room[8].mine = true;
+room[13].mine = true;
+
+console.log("4:10");
+
+function displayRecords(){
+  let j = 0;
+  for (let i = 1; i < 17; i++) {
+    if (room[i].mine == true){
+      j = j + 1;
+      console.log(i,j);
+      createRecord(i,j);
+    }
+  }
+}
+
+function createRecord(i,j){
+  var tree2 = document.createDocumentFragment();
+  var x = document.createElement("button");
+  x.innerHTML = "Room "+String(i);
+  x.setAttribute("onclick","deleteRoom(i)")
+  tree2.appendChild(x);
+  var x = document.createElement("table");
+  x.innerHTML = '<tr><td><p id = "demo"'+String(j)+'"col1"></p></td><td><p id = "demo"'+String(j)+'"col2"></p></td></tr>';
+  tree2.appendChild(x);
+  tree2.appendChild(createTextbox());
+  console.log(document.documentElement.innerHTML);
+  document.getElementById("demo"+String(j)).appendChild(tree2);
+  console.log(document.documentElement.innerHTML);
+ 
+  var tree = document.createDocumentFragment();
+  var x = document.createElement("h3");
+  x.innerHTML = "Work up";
+  tree.appendChild(x);
+  t = createCheckbox(i,room[i].wu_orders,"Orders");
+  tree.appendChild(t);
+  t= createCheckbox(i,room[i].wu_workuppending,"Work up");
+  tree.appendChild(t);
+  if (room[i].wu_workuppending == true){
+//    tree.appendChild(indent());
+    t =createCheckbox(i,room[i].wu_labspending,"Labs");
+    tree.appendChild(t);
+//    tree.appendChild(indent());
+    t =createCheckbox(i,room[i].wu_imagingpending,"Imaging");
+    tree.appendChild(t);
+//    tree.appendChild(indent());
+    t =createCheckbox(i,room[i].wu_procedurepending,"Procedure");
+    tree.appendChild(t);
+  }
+  t =createCheckbox(i,room[i].consult,"Consult");
+  tree.appendChild(t);
+  t= createCheckbox(i,room[i].wu_admit,"Admit");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].wu_discharge,"Discharge");
+  tree.appendChild(t);
+  if (room[i].wu_admit == true) {
+//    tree.appendChild(indent());
+    t = createCheckbox(i,room[i].admit_discharge,"Discharge");
+    tree.appendChild(t);
+//    tree.appendChild(indent());
+    t = createCheckbox(i,room[i].admit_order,"Order");
+    tree.appendChild(t);
+ //   tree.appendChild(indent());
+    t = createCheckbox(i,room[i].admit_bedrequest,"Bed request");
+    tree.appendChild(t);
+  }
+  if (room[i].wu_discharge == true) {
+ //   tree.appendChild(indent());
+    t = createCheckbox(i,room[i].discharge_dx,"Diagnoses");
+    tree.appendChild(t);
+//    tree.appendChild(indent());
+    t = createCheckbox(i,room[i].discharge_prescriptions, "Prescriptions");
+    tree.appendChild(t);
+  //  tree.appendChild(indent());
+    t = createCheckbox(i,room[i].discharge_referral,"Referral");
+    tree.appendChild(t);
+ //   tree.appendChild(indent());
+    t = createCheckbox(i,room[i].discharge_instructions,"Instructions");
+    tree.appendChild(t);
+  //  tree.appendChild(indent());
+    t = createCheckbox(i,room[i].discharge_outptorders,"Outpatient Orders");
+    tree.appendChild(t);
+ //   tree.appendChild(indent());
+    t = createCheckbox(i,room[i].discharge_worknote,"Work Note");
+    tree.appendChild(t);
+  }
+  
+  var z = document.documentElement.innerHTML;
+  console.log(z);
+  var y = document.getElementById("demo"+String(j)+"col1")
+  y.appendChild(tree);
+  var tree = document.createDocumentFragment();
+  var x = document.createElement("h3");
+  x.innerHTML = "Note";
+  tree.appendChild(x);
+  t = createCheckbox(i,room[i].note_hpi,"HPI");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_consult,"Consult");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_mdm,"MDM");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_ekg,"EKG");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_disposition,"Disposition");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_sign,"Sign");
+  tree.appendChild(t);
+  document.getElementById("demo"+String(j)+"col2").appendChild(tree);
+  
+  
+  
+  
+  
+ 
+}
+
+function createCheckbox(i,flag,label) {
+  var t = document.createDocumentFragment();
+  var x = document.createElement("INPUT");
+  x.setAttribute("type", "checkbox");
+  x.setAttribute("checked",!flag);
+  x.setAttribute("label",label);
+  var id = "rm"+String(i)+":"+label;
+  x.setAttribute("id",id);
+  x.setAttribute("name",i);
+  x.setAttribute("onchange","checkboxChanged(this.checked,this.label, this.name)");
+  t.appendChild(x);
+  var x = document.createElement("label");
+  x.htmlFor = id;
+  x.innerHTML = label;
+  t.appendChild(x);
+  var x = document.createElement("br");
+  t.appendChild(x);
+  return(t);
+  
+}
+
+function createTextbox(i,label) {
+  var x = document.createElement("textarea");
+  x.setAttribute("rows","10");
+  x.setAttribute("cols","20");
+  x.setAttribute("wrap","soft");
+  x.setAttribute("onchange","recordTextboxValue(i,this.value)");
+  x.setAttribute("id","rm"+String(i)+"_dx");
+  return(x);
+}
+
+function checkboxChanged(value, label, i) {
+  switch(label) {
+    case "Work Up":
+      room[i].wu_workuppending = value;
+      break;
+    case "Orders":
+      room[i].wu_orders = value;
+      break;
+    case "Labs":
+      room[i].wu_labspending = value;
+      break;
+    case "Imaging":
+      room[i].wu_imagingpending = value;
+      break;
+    case "Procedure":
+      room[i].wu_procedurepending = value;
+      break;
+    case "Consult":
+      room[i].wu_consult = value;
+      break;
+    case "Admit":
+      room[i].wu_admit = value;
+      room[i].wu_discharge = !value;
+      break;
+    case "Discharge":
+      room[i].wu_discharge = value;
+      room[i].wu_admit = !value;
+      break;
+  }
+  
+}
+
+function pickRoom(i){
+  room[i].mine = true;
+  displayRecords();
+}
+
+function deleteRoom(i){
+  room[i].mine = false;
+  displayRecords();
+}
+
+function recordTextboxValue(i,value){
+  room[i].wu_dx = value;
+}
+
+function indent(){
+  var t = document.createElement("pre");
+  t.innerHTML = "     ";
+  return(t);
+}
+
+displayRecords();
---
+++ Work284/records11.js
@@ -0,0 +1,431 @@
+//Improvements:
+
+// done - Put work up and note in 2 columns
+// done - Add textbox for dx, problems
+//  done - formatting:  don't vertically center
+//  done - formatting:  buttons to left side
+//  done - release room with button
+//  DONE - put dx in textarea
+//  done - more table cells
+//  done - larger text area
+//  done = larger buttons
+//  done - document ct
+//  done - note consult if consult checked
+//  done - box for ekg
+//  done - time activities
+//  done - Special:  chest pain, sepsis, DKA, pancreatitis, stroke
+//  done - continue negative countdown
+//  done - needs space after comma
+//  done - bold numbers on activity
+//  done - split awaiting into task and time separately
+
+
+//  *Pending labs:  UA, repeat troponin 
+//  done - EKG disappears
+//  *national IV shortage
+//  done - change text color
+//  done - to do tasks:  print labs, prescriptions, work note, consult, 
+//  done - buttons to show what needs done on pt:  see patient, consult, meds, labs, procedure, work note, prescription
+//  done - separate box for plan
+//  done = button to red
+
+//  save to file
+//  IV contrast not used
+//  conditions:  admitting, neuro, night, number of patients
+//  list awaiting at bottom of each room
+//  diagnosis at the top
+//  add to list of charts to finish
+
+
+
+//  MDM:
+//	Sepsis
+//	Heart score
+//	Pancreatitis
+//	Stroke
+
+
+var newest = 0;
+var activeElement = 'none';
+var all_dx = "";
+var all_plans = "";
+var pt_count = 0;
+const roomCount = 24;
+
+function Room() {
+  this.mine = false;
+  this.wu_orders =  true;
+  this.wu_workuppending =  false;
+  this.wu_labspending =  true;
+  this.wu_ua = false;
+  this.wu_trop = false;
+  this.wu_EKGresult = "";
+  this.wu_imagingpending =  true;
+  this.wu_CTresult = "";
+  this.wu_procedurepending = false;
+  this.wu_consult = false;
+  this.wu_disposition = false;
+  this.wu_admit = false;
+  this.wu_discharge = false;
+  this.admit_bedrequest = true;
+  this.admit_discharge = true;
+  this.admit_order = true;
+  this.discharge_dx = true;
+  this.discharge_instructions = true;
+  this.discharge_prescriptions = true;
+  this.discharge_referral = true;
+  this.discharge_outptorders = true;
+  this.discharge_worknote = true;
+  this.wu_problems = "";
+  this.wu_dx = "";
+  this.wu_plan = "";
+  this.note_hpi = true;
+  this.note_ros = true;
+  this.note_pe = true;
+  this.note_course = true;
+  this.note_consult = false;
+  this.note_procedure = false;
+  this.note_mdm = true;
+  this.note_ct = false;
+  this.note_ekg = true;
+  this.note_disposition = true;
+  this.note_sign = true;
+  this.awaiting = [];
+}
+
+class Awaiting {
+	constructor(activity,t){
+		this.label = activity;
+		this.minutes = t;
+	}
+}
+
+class AwaitingList {
+	constructor(activity,t,room,c){
+       		this.activity = activity;
+		this.minutes = t;
+		this.room = room;
+		this.clr = c;
+	}
+	label() {
+		return("<b>"+String(this.minutes) +"</b> min - Rm <b>"+String(this.room)+"</b>: "+this.activity);
+	}
+	color() {
+		return(['black','blue','red'][this.clr]);
+	}
+}
+
+
+
+
+
+const room = [];
+const default_room = new Room;
+
+for (let i = 1; i < roomCount; i++){
+  room[i] = new Room;
+}
+
+
+function displayRecords(){
+
+	
+  let j = 0;
+  for (let i = 1; i < roomCount; i++) {
+    if (room[i].mine == true){
+      j = j + 1;
+      createRecord(i,j);
+    }
+  }
+  if (activeElement == 'none') {
+  document.getElementById("rm"+String(newest)+"_dx").focus();
+  }
+  else {
+    document.getElementById(activeElement).focus();
+  }
+}
+
+
+function createRecord(i,j){
+  var tree2 = document.createDocumentFragment();
+  var x = document.createElement("button");
+	if (i < 23) {
+  		x.innerHTML = "Room "+String(i);
+  }
+  else {
+    x.innerHTML = "Hallway";
+  }
+  x.setAttribute("class","button2");
+  x.setAttribute("name",i);
+ // x.setAttribute("width","70");
+  x.setAttribute("onclick","deleteRoom(this.name)");
+  tree2.appendChild(x);
+  var x = document.createElement("table");
+  x.innerHTML = '<tr><td style="vertical-align:  top;"><p id = "demo'+String(j)+'col1"></p></td><td style="vertical-align:  top;"><p id = "demo'+String(j)+'col2"></p></td></tr>';
+  tree2.appendChild(x);
+  tree2.appendChild(createTextbox(i));
+
+  document.getElementById("demo"+String(j)).appendChild(tree2);
+ 
+
+ 
+}
+
+function createCheckboxes(){
+
+  var tree = document.createDocumentFragment();
+  t = createCheckbox(i,room[i].wu_orders,"Orders");
+  tree.appendChild(t);
+  t= createCheckbox(i,room[i].wu_workuppending,"Work up");
+  tree.appendChild(t);
+  if (room[i].wu_workuppending == true){
+  //  tree.appendChild(indent());
+    t =createCheckbox(i,room[i].wu_labspending,"&nbsp;&nbsp;&nbsp;Labs");
+    tree.appendChild(t);
+//    tree.appendChild(indent());
+    t =createCheckbox(i,room[i].wu_imagingpending,"&nbsp;&nbsp;&nbsp;Imaging");
+    tree.appendChild(t);
+//    tree.appendChild(indent());
+    t =createCheckbox(i,room[i].wu_procedurepending,"&nbsp;&nbsp;&nbsp;Procedure");
+    tree.appendChild(t);
+  }
+  t =createCheckbox(i,room[i].wu_consult,"Consult");
+  tree.appendChild(t);
+  t= createCheckbox(i,room[i].wu_admit,"Admit");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].wu_discharge,"Discharge");
+  tree.appendChild(t);
+  if (room[i].wu_admit == true) {
+//    tree.appendChild(indent());
+    t = createCheckbox(i,room[i].admit_discharge,"Discharge ");
+    tree.appendChild(t);
+//    tree.appendChild(indent());
+    t = createCheckbox(i,room[i].admit_order,"Order");
+    tree.appendChild(t);
+ //   tree.appendChild(indent());
+    t = createCheckbox(i,room[i].admit_bedrequest,"Bed request");
+    tree.appendChild(t);
+  }
+  if (room[i].wu_discharge == true) {
+ //   tree.appendChild(indent());
+    t = createCheckbox(i,room[i].discharge_dx,"Diagnoses");
+    tree.appendChild(t);
+//    tree.appendChild(indent());
+    t = createCheckbox(i,room[i].discharge_prescriptions, "Prescriptions");
+    tree.appendChild(t);
+  //  tree.appendChild(indent());
+    t = createCheckbox(i,room[i].discharge_referral,"Referral");
+    tree.appendChild(t);
+ //   tree.appendChild(indent());
+    t = createCheckbox(i,room[i].discharge_instructions,"Instructions");
+    tree.appendChild(t);
+  //  tree.appendChild(indent());
+    t = createCheckbox(i,room[i].discharge_outptorders,"Outpatient Orders");
+    tree.appendChild(t);
+ //   tree.appendChild(indent());
+    t = createCheckbox(i,room[i].discharge_worknote,"Work Note");
+    tree.appendChild(t);
+  }
+  
+  var y = document.getElementById("demo"+String(j)+"col1")
+  y.appendChild(tree);
+ /* var tree = document.createDocumentFragment();
+  t = createCheckbox(i,room[i].note_hpi,"HPI");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_course,"Course");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_consult,"Consult ");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_procedure,"Procedure ");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_mdm,"MDM");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_ct, "CT ");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_ekg,"EKG");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_disposition,"Disposition");
+  tree.appendChild(t);
+  t = createCheckbox(i,room[i].note_sign,"Sign");
+  tree.appendChild(t);
+  document.getElementById("demo"+String(j)+"col2").appendChild(tree);*/
+}
+
+function createCheckbox(i,flag,label) {
+  var t = document.createDocumentFragment();
+  var x = document.createElement("INPUT");
+  x.setAttribute("type", "checkbox");
+ // x.setAttribute("autocomplete","off");
+  if (flag) {
+	x.setAttribute("checked",true);
+  }
+  else {
+	x.removeAttribute("checked");
+  }
+  x.setAttribute("label",label);
+  var id = "rm"+String(i)+":"+label;
+  x.setAttribute("id",label+"Checkbox"+String(i));
+  x.setAttribute("name",label);
+  x.setAttribute("onchange","checkboxChanged(this.checked,this.name,"+String(i)+")");
+  t.appendChild(x);
+  var x = document.createElement("label");
+  x.htmlFor = i;
+  x.innerHTML = label;
+  t.appendChild(x);
+  var x = document.createElement("br");
+  t.appendChild(x);
+  return(t);
+  
+}
+
+function createLabel(i,text){
+  var x = document.createElement("label");
+  x.setAttribute("label",text);
+  return(x);
+}
+
+function createTextbox(i) {
+  var t = document.createDocumentFragment();
+  var x = document.createElement("br");
+  t.appendChild(x);
+  var x = document.createElement("label");
+  x.setAttribute("for","rm"+String(i)+"_dx");
+  x.innerHTML = "Problems/diagnoses:";
+  t.appendChild(x);
+  var x = document.createElement("br");
+  t.appendChild(x);
+  // Textbox for Problems/Dx:
+  var x = document.createElement("textarea");
+  x.setAttribute("rows","10");
+  x.setAttribute("cols","25");
+  x.setAttribute("wrap","soft");
+  x.setAttribute("onchange","recordTextboxValue(this.name,this.value)");
+  x.setAttribute("id","rm"+String(i)+"_dx");
+  x.setAttribute("name",i);
+  x.value = room[i].wu_dx;
+  t.appendChild(x);
+
+
+//Plan box
+  var x = document.createElement("br");
+  t.appendChild(x);
+  var x = document.createElement("label");
+  x.setAttribute("for","rm"+String(i)+"_dx");
+  x.innerHTML = "Plan:";
+  t.appendChild(x);
+  var x = document.createElement("br");
+  t.appendChild(x);
+  var x = document.createElement("textarea");
+  x.setAttribute("rows","10");
+  x.setAttribute("cols","25");
+  x.setAttribute("wrap","soft");
+  x.setAttribute("onchange","recordPlanValue(this.name,this.value)");
+  x.setAttribute("id","rm"+String(i)+"_plan");
+  x.setAttribute("name",i);
+  x.value = room[i].wu_plan;
+  t.appendChild(x);
+
+}
+
+
+function pickRoom(i){
+  room[i] = {...default_room};  //used to deep copy default values
+  room[i].mine = true;
+  newest = i;
+  activeElement = 'none';
+  pt_count++;
+  toDoList[i] = [];
+  toDoButtonClicked(i,"SEE PATIENT");
+  toDoButtonClicked(i,"ORDER LABS");
+  toDoButtonClicked(i,"ORDER MEDS");
+  refreshTable();
+  displayRecords();
+  document.getElementById("pt_count").innerHTML = pt_count;
+}
+
+function deleteRoom(i){
+  all_dx = all_dx + "\n"+document.getElementById("rm"+String(i)+"_dx").value;
+  all_plans = all_plans + "\n" + document.getElementById("rm"+String(i)+"_plan").value;
+  room[i].mine = false;
+  toDoList[i] = [];
+  document.getElementById('dx').innerHTML = all_dx;
+  document.getElementById('plans').innerHTML = all_plans; //all_plans;
+	
+  refreshTable();
+  displayRecords();
+}
+
+
+function refreshTable(){
+  var x = document.getElementById('awaitListPlaceholder');
+  x.innerHTML = "";
+  var x = document.getElementById('table1');
+  x.innerHTML = '      <tr>\
+        <td class = "td1"> <p id="demo1"></p> </td>\
+        <td class = "td1"> <p id="demo2"></p> </td>\
+        <td class = "td1"> <p id="demo3"></p> </td>\
+        <td class = "td1"> <p id="demo4"></p> </td>\
+        <td class = "td1"> <p id="demo5"></p> </td>\
+        <td class = "td1"> <p id="demo6"></p> </td>\
+      </tr>\
+      <tr>\
+        <td class = "td1"> <p id="demo7"></p> </td>\
+        <td class = "td1"> <p id="demo8"></p> </td>\
+        <td class = "td1"> <p id="demo9"></p> </td>\
+        <td class = "td1"> <p id="demo10"></p> </td>\
+        <td class = "td1"> <p id="demo11"></p> </td>\
+        <td class = "td1"> <p id="demo12"></p> </td>\
+      </tr>';
+/*	<h3>Diagnoses</h3\
+    <pre id="dx" /><br><br>\
+	<h3>Plans</h3>\
+    <pre id="plans" /><br>'; */
+}
+
+function recordTextboxValue(i,value){
+  room[i].wu_dx = value;
+}
+
+function recordPlanValue(i,value){
+  room[i].wu_plan = value;
+}
+
+function indent(){
+  var t = document.createElement("h6");
+  t.innerHTML = "text";
+  return(t);
+}
+
+function sortArray(){
+	awaitingList.sort(function(a,b){return(a.minutes - b.minutes)});
+}
+
+
+function copyAdmitDoc(){
+	var dr = document.getElementById("physicians").value;
+	var x = dr.split(",");
+	navigator.clipboard.writeText("At "+currentTime() + ", I discussed the situation with Dr. "+x[0]+" who will admit");
+}
+
+// Parse timestamp in YYYY-MM-DDTHH:mm:ss format as local
+function parseDateString(s) {
+  var b = s.split(/\D/g);
+  return new Date(b[0], --b[1], b[2], b[3], b[4], b[5], 0);
+}
+
+// Format time as hh:mm AM/PM
+function formatHHMM(date) {
+  function z(n) {
+    return (n < 10 ? '0' : '') + n;
+  }
+  var h = date.getHours();
+  return z(h % 12 || 12) + ':' + z(date.getMinutes()) + ' ' + (h < 12 ? 'AM' : 'PM');
+}
+
+function currentTime(){
+//	return(formatHHMM(parseDateString(Date.now())));
+	var x = Date().toLocaleString();
+	return(x.slice(16,21));
+}
+		
+		
